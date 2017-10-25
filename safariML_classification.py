721a2286368fe0d0166c0ff3f9a5d0af19daef46
HONG <soojung.hong@atos.net>
2017-10-25 17:09:46 +0200

    initial commit

---
+++ safariML_classification.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 25 16:59:21 2017
+
+@author: a613274
+@Safari ML, Chapter 3 : Classification
+https://www.safaribooksonline.com/library/view/hands-on-machine-learning/9781491962282/ch03.html
+"""
+
+#-------------
+# Fetch data
+#------------
+from sklearn.datasets import fetch_mldata
+mnist = fetch_mldata('MNIST original')
+mnist 
+
+
