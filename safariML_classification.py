470fb694ff0300d816fae068966fde906fdb82c6
Soojung <hong.soojung@gmail.com>
2017-10-28 16:48:19 +0200

    testing SGD classifier

--- safariML_classification.py
+++ safariML_classification.py
@@ -10,16 +10,28 @@ https://www.safaribooksonline.com/library/view/hands-on-machine-learning/9781491
 #-------------------------------------------
 # Fetch data
 #-------------------------------------------
+
+"""
+# on my Mac, this doesn't work. Use code below 
 from sklearn.datasets import fetch_mldata
 mnist = fetch_mldata('MNIST original')
 mnist 
+"""
+
+from sklearn.datasets.mldata import fetch_mldata
+import tempfile
+test_data_home = tempfile.mkdtemp()
+
+mnist = fetch_mldata('MNIST original', data_home=test_data_home)
+mnist 
+
 print(type(mnist))
 
 #------------------------------
 # check how data looks like 
 X, y = mnist["data"], mnist["target"]
 X
-X.shape
+X.shape #(70000, 784) - 70000 images, 784 features (28 * 28 pixels)
 y
 y.shape
 
@@ -52,4 +64,36 @@ import numpy as np
 #-------------------------------------------------------------------
 # shuffle index so, the cross validation folds will not be similar
 shuffle_index = np.random.permutation(60000)
+shuffle_index
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+# target vector this classifier
+y_train_5 = (y_train == 5)  # True for all 5s, False for all other digits.
+y_test_5 = (y_test == 5)
+
+# pick a classifier and train it
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+sgd_clf.predict([some_digit])
+
+#------------------------------------------------------------------------------------------------
+# implement 'Cross Validation' process which does same thing as Scikit-Learn's cross_val_score()
+#------------------------------------------------------------------------------------------------
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42)
+print(type(skfolds)) #skfolds is class 
+
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))  # prints 0.9502, 0.96565 and 0.96495
