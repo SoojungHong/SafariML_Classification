5ec33068816e4835f1621f685db2089d353b73df
HONG <soojung.hong@atos.net>
2017-11-21 17:19:40 +0100

    learning curve

--- ch4_training_models.py
+++ ch4_training_models.py
@@ -123,3 +123,30 @@ lin_reg.intercept_, lin_reg.coef_
 y_predict = lin_reg.predict(X_poly)
 y_predict
 
+#-------------------------------------------------------------
+# learning curve : measuring the generalization performance 
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+def plot_learning_curves(model, X, y):
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+    train_errors, val_errors = [], []
+    for m in range(1, len(X_train)):
+        model.fit(X_train[:m], y_train[:m])
+        y_train_predict = model.predict(X_train[:m])
+        y_val_predict = model.predict(X_val)
+        train_errors.append(mean_squared_error(y_train_predict, y_train[:m]))
+        val_errors.append(mean_squared_error(y_val_predict, y_val))
+    plt.plot(np.sqrt(train_errors), "r-+", linewidth=2, label="train")
+    plt.plot(np.sqrt(val_errors), "b-", linewidth=3, label="val")
+
+#learning curve of the plain Linear Regression model
+lin_reg = LinearRegression()
+plot_learning_curves(lin_reg, X, y) 
+
+
+from sklearn.pipeline import Pipeline
+
+polynomial_regression = Pipeline([("poly_features", PolynomialFeatures(degree=10, include_bias=False)), ("lin_reg", LinearRegression()),]) 
+polynomial_regression
+plot_learning_curves(polynomial_regression, X, y)
